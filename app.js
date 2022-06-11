c9c4b1b2547050d90ecc9eabc4a63bc06838e0c6
Valentin Sznitowski <78902757+sznitowski@users.noreply.github.com>
2022-06-11 11:18:41 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const router = require("./routes");
+const AppError = require("./utils/appError");
+const errorHandler = require("./utils/errorHandler");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+app.use(router);
+
+//app.use(bodyParser.json());
+
+app.all("*", (req, res, next) => {
+ next(new AppError(`The URL ${req.originalUrl} does not exists`, 404));
+});
+app.use(errorHandler);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+ console.log(`server running on port ${PORT}`);
+});
+
+
+module.exports = app;
